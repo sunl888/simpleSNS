b76dc815a0bfe21ac372c61d98aa0b6825168d1e
wqer1019 <2013855675@qq.com>
2018-03-08 01:08:53 +0800

    update commit

--- app/Transformers/ImageTransformer.php
+++ app/Transformers/ImageTransformer.php
@@ -21,13 +21,13 @@ class ImageTransformer extends BaseTransformer
         }
 
         return [
-            'hash'      => $image->hash,
-            'mime'      => $image->mime,
-            'width'     => $image->width,
-            'height'    => $image->height,
-            'url'       => $image->getUrl(),
-            'cover_url' => $image->getUrl('cover_url'),
-            'is_gif'    => str_contains($image->mime, 'gif'),
+            'hash' => $image->hash,
+            'mime' => $image->mime,
+            'width' => $image->width,
+            'height' => $image->height,
+            'url' => image_url($image->hash, $image->ext),
+            'cover_url' => image_url($image->hash, $image->ext, 'cover_url'),
+            'is_gif' => str_contains($image->mime, 'gif'),
             // 高宽比大于等于 3 认为是长图
             'is_high' => $image->height / $image->width >= 3,
         ];
--- init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-php simpleSNS/artisan migrate --seed && php-fpm
